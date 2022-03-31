126f158b7c7590596c878f016806a1e0e1eae380
unknown <ivanalejandroherrera2000@gmail.com>
2022-03-31 12:36:28 -0300

    Milestones component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "components/Home.js";
 import Portfolio from "components/Portfolio";
 import Services from "components/Services";
+import Milestones from "components/Milestones";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <Services />
       <Portfolio />
+      <Milestones />
     </div>
   );
 }
---
+++ client/src/components/Milestones.js
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import milestone1 from "assets/milestone1.png";
+import milestone2 from "assets/milestone2.png";
+import milestone3 from "assets/milestone3.png";
+import milestoneBackround from "assets/milestone-background.png";
+
+function Milestones() {
+  const milestone = [
+    {
+      image: milestone1,
+      data: "Cleints Served",
+      amount: "407",
+    },
+    {
+      image: milestone2,
+      data: "of raw data",
+      amount: "10T",
+    },
+    {
+      image: milestone3,
+      data: "recommendation",
+      amount: "235",
+    },
+  ];
+
+  return (
+    <Section>
+      <div className="background">
+        <img src={milestoneBackround} alt="Milestone Background" />
+      </div>
+      <div className="milestones">
+        {milestone.map(({ image, data, amount }) => {
+          return (
+            <div className="milestone">
+              <p>{amount}</p>
+              <span>{data}</span>
+              <img src={image} alt="milestone" />
+            </div>
+          );
+        })}
+      </div>
+    </Section>
+  );
+}
+
+const Section = styled.section`
+  height: 100vh;
+  background-color: var(--primary-color);
+  padding: 0 10rem;
+  position: relative;
+  overflow: hidden;
+
+  .background {
+    position: absolute;
+    left: 0;
+    bottom: -30%;
+    img {
+      z-index: 2;
+      height: 43rem;
+    }
+  }
+
+  .milestones {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    color: white;
+    align-items: center;
+    height: 100%;
+    .milestone {
+      z-index: 20;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 2rem;
+      p {
+        font-size: 5rem;
+        font-weight: bolder;
+        line-height: 3rem;
+      }
+      span {
+        text-transform: uppercase;
+        color: #ffffffc7;
+      }
+      img {
+        height: 6rem;
+      }
+    }
+  }
+`;
+
+export default Milestones;
