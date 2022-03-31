f21cbae7e171989f91204a88eadc5206f1b8e054
unknown <ivanalejandroherrera2000@gmail.com>
2022-03-31 12:27:29 -0300

    Portfolio component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import Home from "components/Home.js";
-import Services from "components/Services";
 import React from "react";
+import Home from "components/Home.js";
+import Portfolio from "components/Portfolio";
+import Services from "components/Services";
 
 function App() {
   return (
     <div>
       <Home />
       <Services />
+      <Portfolio />
     </div>
   );
 }
